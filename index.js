3bc00c0c15e10a43b8b11f18af441f0a8531a624
StevenLow <106484271+Steven-Low@users.noreply.github.com>
2023-12-21 10:30:43 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-const express = require('express');
-const { exec } = require('child_process');
+// Replace the require statement with dynamic import
+import('node-fetch')
+  .then((module) => {
+    const fetch = module.default;
 
-const fetch = import('node-fetch');
-const fs = require('fs');
+    const express = require('express');
+    const { exec } = require('child_process');
+    const fs = require('fs');
 
-const downloadURL = 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp';
+    const downloadURL = 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp';
 
-const downloadYTDL = async () => {
-  try {
-    const response = await fetch(downloadURL);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const fileStream = fs.createWriteStream('yt-dlp'); // Save the file as 'yt-dlp' locally
-    await new Promise((resolve, reject) => {
-      response.body.pipe(fileStream);
-      response.body.on('error', reject);
-      fileStream.on('finish', resolve);
-    });
-    console.log('yt-dlp downloaded successfully!');
-  } catch (error) {
-    console.error('Error downloading yt-dlp:', error.message);
-  }
-};
-
-downloadYTDL();
-const app = express();
-
-app.get('/getAudioURL/:videoID', (req, res) => {
-    const { videoID } = req.params;
-    const url = `https://www.youtube.com/watch?v=${videoID}`;
-    const command = `yt-dlp --extract-audio --audio-format mp3 --get-url ${url}`;
-    
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            res.status(500).send('Error fetching audio URL');
-            return;
+    const downloadYTDL = async () => {
+      try {
+        const response = await fetch(downloadURL);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const audioURL = stdout.trim();
-        res.json({ audioURL });
+        const fileStream = fs.createWriteStream('yt-dlp'); // Save the file as 'yt-dlp' locally
+        await new Promise((resolve, reject) => {
+          response.body.pipe(fileStream);
+          response.body.on('error', reject);
+          fileStream.on('finish', resolve);
+        });
+        console.log('yt-dlp downloaded successfully!');
+      } catch (error) {
+        console.error('Error downloading yt-dlp:', error.message);
+      }
+    };
+
+    downloadYTDL();
+
+    // Rest of your code for setting up the Express app and API endpoints...
+    const app = express();
+
+    app.get('/getAudioURL/:videoID', (req, res) => {
+        const { videoID } = req.params;
+        const url = `https://www.youtube.com/watch?v=${videoID}`;
+        const command = `yt-dlp --extract-audio --audio-format mp3 --get-url ${url}`;
+        
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                res.status(500).send('Error fetching audio URL');
+                return;
+            }
+            const audioURL = stdout.trim();
+            res.json({ audioURL });
+        });
     });
-});
 
-// Other necessary routes and configurations...
+    // Other necessary routes and configurations...
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+
+  })
+  .catch((error) => {
+    console.error('Error importing node-fetch:', error);
+  });
