e7d2d15d72b1f05bbd249ab0926109b0f6c09dfa
StevenLow <106484271+Steven-Low@users.noreply.github.com>
2023-12-21 14:45:12 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import yt_dlp
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/getAudioURL/<videoID>')
+def get_youtube_audio(videoID):
+    # Create yt-dlp options
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '128',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        url = f"https://www.youtube.com/watch?v={videoID}"
+        info = ydl.extract_info(url, download=False)     # Get video info
+        audio_url = info['url']                          # Extract audio URL    
+        return jsonify({'audioURL': audio_url})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')  # Important for Render deployment
